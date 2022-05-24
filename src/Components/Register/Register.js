66aa8ecee9a3906edcab0e3e8d88525365cbf801
tanimpersonal <tashintanim2019@gmail.com>
2022-05-24 22:06:26 +0600

    email verification

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,19 +1,27 @@
+import { sendEmailVerification } from "firebase/auth";
 import React, { useRef, useState } from "react";
 
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Register = () => {
+  const navigate = useNavigate();
   const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
   const email = useRef();
   const password = useRef();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
   const handleRegister = (event) => {
     event.preventDefault();
     console.log(email.current.value, password.current.value);
 
     createUserWithEmailAndPassword(email.current.value, password.current.value);
   };
+  if (user) {
+    navigate(from, { replace: true });
+  }
   return (
     <div>
       <div className="register">
