8fa7f13f2a5a748d62085f8e8dea2c46b8ae3c55
tanimpersonal <tashintanim2019@gmail.com>
2022-05-24 23:38:17 +0600

    spinner added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -28,7 +28,61 @@ const Home = () => {
           </div>
         </div>
       ) : (
-        <div>Loading...</div>
+        <div>
+          <div class="flex justify-center items-center space-x-2">
+            <div
+              class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-600"
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div
+              class="
+      spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
+        text-purple-500
+      "
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div
+              class="
+      spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
+        text-green-500
+      "
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div
+              class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-red-500"
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div
+              class="
+      spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0
+        text-yellow-500
+      "
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div
+              class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-blue-300"
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+            <div
+              class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-gray-300"
+              role="status"
+            >
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
